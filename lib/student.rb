29a95eec678c0fac4bc4fe8c2a7561d42552b047
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-25 17:51:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,5 +35,6 @@ class Student
       VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
 end
