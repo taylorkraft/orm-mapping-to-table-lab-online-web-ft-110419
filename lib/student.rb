a973d9ae3a4a84ba9b52fe8c78de242707d19df0
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-25 17:59:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,8 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
 
-    def self.create(name)
-      student = Student.new(name)
+    def self.create({:name, :grade})
+      student = Student.new(name, grade)
       student.save
       student
     end
