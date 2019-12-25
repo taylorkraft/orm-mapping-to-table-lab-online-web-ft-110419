4993c563067d40c97303f476e51f86dc10af369b
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-25 17:37:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,6 @@ class Student
       INSERT INTO students(name, grade)
       VALUES (?, ?)
       SQL
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql, self.name, self.grade)
     end
 end
