32023541186acf838f5a13bbb6651970f064cd4c
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-25 16:16:31 +0000

    initialize, create table methods

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,24 @@
 class Student
+    attr_accessor :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+    def initialize(name, grade, id = nil)
+      @id = id
+      @name = name
+      @grade = grade
+    end
+
+    def id
+      @id
+    end
+
+    def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+      SQL
+      DB[:conn].execute(sql)
+    end
 end
